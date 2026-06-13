bd0d944802e6b1d3753a9ad546285fb9925e8b11
Nikolay <mattafixrus95@mail.ru>
2026-06-14 00:57:43 +0300

    Подключить реальные звуки животных и машин вместо TTS

--- src/data/animals.js
+++ src/data/animals.js
@@ -1,13 +1,13 @@
-import { speak } from "../lib/audio";
+import { speak, playSound } from "../lib/audio";
 
 const ANIMALS_DOMESTIC = [
-  { name: "Кошка",   emoji: "🐱", sound: "мяу" },
-  { name: "Собака",  emoji: "🐶", sound: "гав" },
-  { name: "Корова",  emoji: "🐮", sound: "му" },
-  { name: "Коза",    emoji: "🐐", sound: "меее" },
-  { name: "Баран",   emoji: "🐑", sound: "беее" },
-  { name: "Лошадь",  emoji: "🐴", sound: "и-го-го" },
-  { name: "Петух",   emoji: "🐓", sound: "кукареку" },
+  { name: "Кошка",   emoji: "🐱", sound: "мяу",      file: "cat.wav" },
+  { name: "Собака",  emoji: "🐶", sound: "гав",      file: "dog.wav" },
+  { name: "Корова",  emoji: "🐮", sound: "му",       file: "cow.wav" },
+  { name: "Коза",    emoji: "🐐", sound: "меее",     file: "goat.wav" },
+  { name: "Баран",   emoji: "🐑", sound: "беее",     file: "sheep.wav" },
+  { name: "Лошадь",  emoji: "🐴", sound: "и-го-го",  file: "horse.wav" },
+  { name: "Петух",   emoji: "🐓", sound: "кукареку", file: "rooster.wav" },
   { name: "Гусь",    emoji: "🦢", sound: "га-га" },
 ];
 const ANIMALS_WILD = [
@@ -25,7 +25,7 @@ const ANIMALS_ALL = [...ANIMALS_DOMESTIC, ...ANIMALS_WILD];
 
 export const ANIMAL_SETS = { domestic: ANIMALS_DOMESTIC, wild: ANIMALS_WILD, all: ANIMALS_ALL };
 
-// Озвучить звук животного через SpeechSynthesis (имитация)
+// Озвучить звук животного: реальная запись, либо имитация через SpeechSynthesis
 export function playAnimalSound(animal) {
-  speak(animal.sound);
+  playSound(animal.file, animal.sound);
 }
